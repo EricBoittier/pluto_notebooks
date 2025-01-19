1c2626f17568abcd74035c595936901ad873c74b
Eric Boittier <ericdavid.boittier@unibas.ch>
2025-01-19 16:53:00 +0100

    mac env working

--- Mini proof.jl
+++ Mini proof.jl
@@ -22,12 +22,15 @@ begin
 end
 
 # ╔═╡ 282bca20-d4f1-11ef-3aa9-4d513b14fb52
-include("C:\\Users\\Eric\\Documents\\GitHub\\pluto_notebooks\\Library.jl") 
-
-# ╔═╡ 7730ccf8-92dd-436d-a6db-822add3d3fcb
-include("C:\\Users\\Eric\\Documents\\GitHub\\pluto_notebooks\\GameScript.jl")
+begin
+	include("Library.jl") 
+	include("GameScript.jl")
+end
 
 # ╔═╡ fafa2131-16a1-43f6-980b-422bdc5a780d
+
+
+# ╔═╡ 7730ccf8-92dd-436d-a6db-822add3d3fcb
 
 
 # ╔═╡ 0a4ffce3-88c4-4252-9c4c-18cd927ecdf8
